88065f09e067c00285e226334362ad3166e128c8
Constantine Tarasenkov <iamvfx@gmail.com>
2021-03-24 20:35:19 +0300

    git revert --no-commit fa1fb7f8bbe00d063e017162781b4995f4d69989..HEAD

--- 3rd/3rd_tools/compile.sh
+++
@@ -1,3 +0,0 @@
-clang   ass2iqe.c   -I../.. -I. -Wno-everything -lm -lpthread -ldl -L. -lassimp -o ass2iqe
-clang++ iqe2iqm.cpp -I../..     -Wno-everything -lm -lpthread -ldl              -o iqe2iqm
-clang   mid2wav.c   -I../..     -Wno-everything -lm -lpthread -ldl              -o mid2wav
--- compile.sh
+++
@@ -1,6 +0,0 @@
-clang test.c         -Wno-everything -lm -ldl -pthread -o test
-clang test_collide.c -Wno-everything -lm -ldl -pthread -o test_collide
-clang test_cubemap.c -Wno-everything -lm -ldl -pthread -o test_cubemap
-clang test_script.c  -Wno-everything -lm -ldl -pthread -o test_script
-clang test_sprite.c  -Wno-everything -lm -ldl -pthread -o test_sprite
-clang test_video.c   -Wno-everything -lm -ldl -pthread -o test_video
