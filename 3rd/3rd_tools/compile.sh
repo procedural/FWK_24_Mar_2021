0297da102bb8f586fd9aca37db645bd23b2852b5
Constantine Tarasenkov <iamvfx@gmail.com>
2021-03-24 17:10:01 +0300

    Add local version of the latest assimp (https://github.com/procedural/assimp5_binaries_ubuntu_16_04)

---
+++ 3rd/3rd_tools/compile.sh
@@ -0,0 +1,3 @@
+clang   ass2iqe.c   -I../.. -I. -Wno-everything -lm -lpthread -ldl -L. -lassimp -o ass2iqe
+clang++ iqe2iqm.cpp -I../..     -Wno-everything -lm -lpthread -ldl              -o iqe2iqm
+clang   mid2wav.c   -I../..     -Wno-everything -lm -lpthread -ldl              -o mid2wav
